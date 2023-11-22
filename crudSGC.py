cef3e5dfc39b8a37ba7d5eade9cd2ebea231563d
DSM-3 <dsm3@fatec.academico>
2023-11-21 22:37:46 -0300

    Adição dos ícones do CRUD

--- crudSGC.py
+++ crudSGC.py
@@ -87,6 +87,12 @@ banco = pymongo.MongoClient("mongodb://localhost:27017/")
 db = banco["banco"]
 collection = db["celulares"]
 
+# Importação de ícones
+ft_salvar = PhotoImage(file=r"icones\salvar.png")
+ft_alterar = PhotoImage(file=r"icones\alterar.png")
+ft_excluir = PhotoImage(file=r"icones\excluir.png")
+ft_procurar = PhotoImage(file=r"icones\procurar.png")
+ft_sair = PhotoImage(file=r"icones\sair.png")
 
 # Componentes da tela
 Label(tela, text="Sistema de Gerenciamento de Celulares", font=("Arial", 18)).grid(row=0, columnspan=2, pady=10)
@@ -121,19 +127,19 @@ lbl_cor.grid(row=6, column=0)
 txt_cor = Entry(tela)
 txt_cor.grid(row=6, column=1, pady=5)
 
-btn_salvar = Button(tela, text='Salvar', font=fonte, command=Salvar)
+btn_salvar = Button(tela, text='Salvar', font=fonte, command=Salvar, image=ft_salvar, compound= TOP, width=60, height=60)
 btn_salvar.grid(row=7, column=0, pady=5, padx=5)
 
-btn_alterar = Button(tela, text='Alterar', font=fonte, command=Alterar)
+btn_alterar = Button(tela, text='Alterar', font=fonte, command=Alterar, image=ft_alterar, compound = TOP, width=60, height=60)
 btn_alterar.grid(row=7, column=1, pady=5, padx=5)
 
-btn_excluir = Button(tela, text='Excluir', font=fonte, command=Excluir)
+btn_excluir = Button(tela, text='Excluir', font=fonte, command=Excluir, image=ft_excluir, compound = TOP, width=60, height=60)
 btn_excluir.grid(row=8, column=0, pady=5, padx=5)
 
-btn_consultar = Button(tela, text='Consultar', font=fonte, command=Consultar)
+btn_consultar = Button(tela, text='Consultar', font=('Arial', 11), command=Consultar, image=ft_procurar, compound = TOP, width=60, height=60)
 btn_consultar.grid(row=8, column=1, pady=5, padx=5)
 
-btn_sair = Button(tela, text='Sair', font=fonte, command=janela.quit)
+btn_sair = Button(tela, text='Sair', font=fonte, command=janela.quit, image = ft_sair, compound = TOP, width=60, height=60)
 btn_sair.grid(row=9, column=0, pady=5, padx=5)
 
 lbl_resultado = Label(tela, text='', font=fonte)
